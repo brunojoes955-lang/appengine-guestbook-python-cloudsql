f6f925a3aa3e396989328a820754ca181f5495ad
m.me/brunojoe1995 <brunojoes955@gmail.com>
2026-04-24 02:11:13 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,1 @@
 
-"""Setting file for the Cloud SQL guestbook"""
-
-CLOUDSQL_INSTANCE = 'ReplaceWithYourInstanceName'
-DATABASE_NAME = 'guestbook'
-USER_NAME = 'ReplaceWithYourDatabaseUserName'
-PASSWORD = 'ReplaceWithYourDatabasePassword'
